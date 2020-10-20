01665469b4c4c7e2204228a718ac27f3d68f1537
David <r.d.jorgensen@gmail.com>
2020-10-19 18:59:05 -0700

    added router and template pages to link to

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from "./login";
+import NavBar from "./navbar";
+import Employee from "./employee";
+import Customer from "./customer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path='/employee/:employeeID' component={Employee} />
+          <Route path='/employee/:employeeID' component={Customer} />
+          <Route path="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ Front-End/src/employee.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Employee extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1>Employee Landing Page</h1> );
+    }
+}
+ 
+export default Employee;
