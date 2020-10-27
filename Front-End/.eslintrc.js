509b2ef581fcec77590adf557c9e407d7e1a3094
Stephen Lam <stephenlam.me@gmail.com>
2020-10-27 14:38:19 -0700

    add (frontend): add eslint and airbnb config

---
+++ Front-End/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb/whitespace',
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    worker: true,
+  },
+};
