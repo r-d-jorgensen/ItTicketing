6de00d354573c4909e2c972c8a2fb88edfa9a5e8
David <r.d.jorgensen@gmail.com>
2021-02-05 13:30:08 -0800

    changed names to dashboards and fixed bugs

--- Front-End/src/entry.jsx
+++ Front-End/src/entry.jsx
@@ -12,18 +12,18 @@ const Login = loadable(() => import('views/Login'));
 const Registration = loadable(() => import('views/Registration'));
 const AccountRetrival = loadable(() => import('views/AccountRetrival'));
 const NotFound = loadable(() => import('views/NotFound'));
-const Customer = loadable(() => import('views/Customer'));
-const Employee = loadable(() => import('views/Employee'));
+const CustomerDashboard = loadable(() => import('views/CustomerDashboard'));
+const EmployeeDashboard = loadable(() => import('views/EmployeeDashboard'));
 
 ReactDOM.render(
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route path="/customer" component={Customer} />
-        <Route path="/employee" component={Employee} />
+        <Route path="/customer/dashboard" component={CustomerDashboard} />
+        <Route path="/employee/dashboard" component={EmployeeDashboard} />
         <Route path="/accountRetrival" component={AccountRetrival} />
         <Route path="/registration" component={Registration} />
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={Login} />ds
         <Route path='/not-found' component={NotFound} />
         <Route path="/" exact component={Home} />
         <Redirect to='/not-found' />
