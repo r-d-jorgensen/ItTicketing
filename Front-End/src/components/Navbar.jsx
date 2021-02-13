a250a912e4d6f4a216c9ff928f1814c9e0adc265
Stephen Lam <stephenlam.me@gmail.com>
2021-02-13 13:06:52 -0800

    update (frontend): change navbar layout/styling

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -2,25 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../App';
 
+import './Navbar.css';
+
 const DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 const Navbar = ({isLogedIn}) => {
   const { dispatch } = React.useContext(AuthContext);
   const NavLinks = () => {
-    let navLandinglinks = [];
-    if (DEVELOPMENT) {
-      navLandinglinks = [
-        { link: '/', name: 'Home' },
-        { link: '/registration', name: 'Registration' },
-        { link: '/accountRetrival', name: 'Account Retrival' },
-      ];
-    } else navLandinglinks = [];
+    const navLandinglinks = DEVELOPMENT ? [
+      { link: '/', name: 'Home' },
+      { link: '/registration', name: 'Registration' },
+      { link: '/accountRetrival', name: 'Account Retrival' },
+    ] : [];
 
     return (
-      <nav>
-        <span className="filler" />
-        <span className="filler" />
-        <span className="filler" />
+      <nav className="nav-links">
         {
           navLandinglinks.map((element) => (
             <Link to={element.link} className="nav-item" key={element.name}>
@@ -28,25 +24,33 @@ const Navbar = ({isLogedIn}) => {
             </Link>
           ))
         }
-        <span className="filler" />
-        <span className="filler" />
-        <span className="filler" />
-        {isLogedIn ?
-          <Link to="/" onClick={() => dispatch({type: 'LOGOUT'})} className="log-button">
-            Logout
-          </Link>
-          :
-          <Link to="/login" className="log-button">
-            Login
-          </Link>}
       </nav>
     );
   };
 
   return (
-    <header className="navbar">
-      <Link to="/">IT Ticketing Systems Inc.</Link>
-      <NavLinks />
+    <header className="header">
+      <div className="navbar">
+        <h1 className="logo">
+          <Link to="/">IT Ticketing Systems Inc.</Link>
+        </h1>
+        <NavLinks />
+        <div className="user-actions">
+          {
+          isLogedIn
+            ? (
+              <Link to="/" onClick={() => dispatch({ type: 'LOGOUT' })} className="nav-item">
+                Logout
+              </Link>
+            )
+            : (
+              <Link to="/login" className="nav-item">
+                Login
+              </Link>
+            )
+          }
+        </div>
+      </div>
     </header>
   );
 };
