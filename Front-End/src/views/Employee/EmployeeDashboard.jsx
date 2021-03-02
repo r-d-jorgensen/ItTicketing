b270d85f20610ccd3dfec1cc6b91a4f9d1df57c0
David <r.d.jorgensen@gmail.com>
2021-03-02 06:10:33 -0800

    skelleton for dashboard

--- Front-End/src/views/Employee/EmployeeDashboard.jsx
+++ Front-End/src/views/Employee/EmployeeDashboard.jsx
@@ -1,17 +1,45 @@
 import React, { Fragment } from 'react';
 import Navbar from 'components/Navbar';
+import './EmployeeDashboard.css';
+
+const activeTickets = [
+  {ticketOwner: 'Homer Stevenson', company: 'Dix-It', title: 'Email not sending attachments',  priority: 'Urgent',
+    details: 'The email is currently not sending attachments that are meant to go with it.'},
+  {ticketOwner: 'John Jacobson', company: 'Big Stuff',  title: 'Stuff',  priority: 'Low',
+    details: 'words about the problem. words about the problem words about the problem words about the problem'},
+  {ticketOwner: 'Bob Levy', company: 'Fix-It', title: 'Unable to add ',  priority: 'mid',
+    details: 'words about the problem. words about the problem words about the problem words about the problem'},
+];
+
+const filters = [
+  'Priority',
+  'Company',
+  'Alphabetical',
+];
 
 function EmployeeDashboard() {
   return (
     <Fragment>
       <Navbar />
       <main id="employee-dashboard">
+        <input type="text" placeholder="Look Up Ticket"/>
         <div id="dashboard-filters">
-          {/*list of filters to change active tickets displyed*/}
+          <h2>Filters</h2>
+          {filters.map((type) =>
+          <div className="filter">
+            <label htmlFor={type}>{type}</label>
+            <input type="radio" id={type} value={type} />
+          </div> )}
         </div>
         <div>
-          <h3>Active Tickets</h3>
-          {/*map function to render active tickets */}
+          <h2>Active Tickets</h2>
+          {activeTickets.map(({title, ticketOwner, company, priority, details}) => 
+          <div className="activeTicket">
+            <h4>{title}</h4>
+            <p>{priority}</p>
+            <p>{company}: {ticketOwner}</p>
+            <p>{details}</p>
+          </div>)}
         </div>
       </main> 
     </Fragment>
