e7db4ebb43351a1db162fa481a7b08db11210379
Stephen Lam <stephenlam.me@gmail.com>
2021-03-06 16:05:59 -0800

    update (backend): refactor helpdesk.js

--- Server/helpdesk.js
+++ Server/helpdesk.js
@@ -1,37 +1,45 @@
+require('dotenv').config({ path: '.env.local' });
+
+const bcrypt = require('bcrypt');
 const mysql = require('mysql');
 const express = require('express');
-const { expressCspHeader, SELF } = require('express-csp-header');
 const jwt = require('jsonwebtoken');
 const expressjwt = require('express-jwt');
 const cookieParser = require('cookie-parser');
+const prexit = require('prexit');
+const cors = require('cors');
+const helmet = require('helmet');
+
 const app = express();
-const serverPort = 80
+
 const validateAuth = expressjwt({
-	secret: 'itT1ck3t1ng',
+	secret: process.env.TICKET_SECRET,
 	credentialsRequired: false,
 	algorithms: ['HS256'],
-	getToken: function fromCookie(req){
-		if(req.cookies.session){
-			return req.cookies.session;
+	getToken ({ cookies }) {
+		if (cookies && cookies.session) {
+			return cookies.session;
 		}
 		return null;
 	}
 });
-app.use(expressCspHeader({directives: {'default-src': [SELF]}}))
+
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+const serverPort = process.env.TICKET_PORT || 80;
 app.listen(serverPort, () => {
 	console.log(`Started server at http://localhost:${serverPort}`);
 });
 
-
-var connection = mysql.createConnection({
-	host: "host",
-	user: "user",
-	password: "Password",
-	port: "port",
-	database: "database",
+const connection = mysql.createConnection({
+	host: process.env.TICKET_DB_HOST,
+	user: process.env.TICKET_DB_USER,
+	password: process.env.TICKET_DB_PASSWORD,
+	port: process.env.TICKET_DB_PORT,
+	database: process.env.TICKET_DB_DATABASE,
 });
 
 connection.connect(function(err) {
@@ -40,61 +48,51 @@ connection.connect(function(err) {
 		return;
 	}
 	console.log("Connected to database.");
-
-	//display variables in ticket_assigned
-	//if(err) throw err;
-	//connection.query("SELECT * FROM ticket_assigned", function(err, result, fields){
-	//if(err) throw err;
-	//console.log(result);
-	//});
-
 });
 
-app.get('/protected', validateAuth, function(req, res){
+app.get('/protected', validateAuth, function(req, res) {
 	if(req.user.email){
 		res.send(`Logged in as ${req.user.email}`)
 	}
 });
 
-app.post('/api/auth', function (req, res){
-	var email = req.body.email
-	var password = req.body.password
+app.post('/api/auth', function apiAuth(req, res) {
+	const { email, password } = req.body;
+	console.log('api/auth', req.body);
 
 	//validate that the incoming email and password are strings
 	//so that they can then be sanitized properly to prevent any
 	//sort of sql injection.
-	if(typeof email != 'string' || typeof password != 'string'){
+	if(typeof email !== 'string' || typeof password !== 'string'){
 		res.status(400).send('Bad Request');
 		return;
 	}
 
-	var escapedEmail = connection.escape(email)
-	var escapedPassword = connection.escape(password)
-	
-	connection.query(`SELECT * FROM tech WHERE password = ${escapedPassword} AND email = ${escapedEmail}`, function(err, result, fields){
-		if(err) throw err;
-		if(result.length){
-			res.cookie('session', jwt.sign({'email':email}, 'itT1ck3t1ng').toString(), { httpOnly: true});
-			res.send({'success': true, 'isEmployee': true});
-		}
-		else{
-			connection.query(`SELECT * FROM user WHERE password = ${escapedPassword} AND email = ${escapedEmail}`, function(err, result, fields){
-				if(err) throw err;
-				if(result.length){
-					res.cookie('session', jwt.sign({'email': email}, 'itT1ck3t1ng').toString(), { httpOnly: true});
-					res.send({'success': true, 'isEmployee': false});
+	// email is escaped internally
+	connection.query('SELECT * FROM  `user` WHERE `email` = ?', [email], (err, [user]) => {
+		if (user) {
+			const { password: pHash, user_type, first_name, last_name, phone_number } = user;
+			bcrypt.compare(password, pHash).then((match) => {
+				if (match) {
+					const token = jwt.sign(email + user.user_id, process.env.TICKET_SECRET);
+					res.cookie('token', token, { httpOnly: true, secure: true });
+					res.json({'success': true, user: {
+						   email,
+						   first_name,
+						   last_name,
+						   phone_number,
+						   user_type,
+						}
+					});
+				} else {
+					res.sendStatus(401);
 				}
-				else{
-					res.send({'success':false});
-				}
-			})
+			});
+		} else {
+			res.sendStatus(401);
 		}
-	})
-})
-
-app.get('/', function(req, res){
-	res.send('ItTicketing API');
-})
+	});
+});
 
 app.use(function(err, req, res, next){
 	console.log(err.name)
@@ -102,4 +100,8 @@ app.use(function(err, req, res, next){
 		console.log('jwt error');
 		next();
 	}
+});
+
+prexit(async () => {
+	connection.end();
 });
