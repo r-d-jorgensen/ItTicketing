8c60aaf83cf20190f0ed73d0d1ade8184a27a1bb
Stephen Lam <stephenlam.me@gmail.com>
2021-04-02 20:09:39 -0700

    update (frontend): add ability to specify `local` or `session` storage

--- Front-End/src/services/useStorage.js
+++ Front-End/src/services/useStorage.js
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react';
 
-export default function useLocalStorage(key) {
+export default function useStorage(key, storageType = 'local') {
   const [value, setValue] = useState(() => {
     try {
-      return JSON.parse(localStorage.getItem(key));
+      if (storageType === 'local') {
+        return JSON.parse(localStorage.getItem(key));
+      // eslint-disable-next-line no-else-return
+      } else if (storageType === 'session') {
+        return JSON.parse(sessionStorage.getItem(key));
+      }
+      return null;
     } catch (error) {
       if (process.env.NODE_ENV === 'development') {
         // eslint-disable-next-line no-console
@@ -14,8 +20,12 @@ export default function useLocalStorage(key) {
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+    if (storageType === 'local') {
+      localStorage.setItem(key, JSON.stringify(value));
+    } else if (storageType === 'session') {
+      sessionStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, storageType, value]);
 
   return [value, setValue];
 }
