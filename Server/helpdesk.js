e69a2db2e9fbc39696ab0ff3f43e8a1d67a56716
Stephen Lam <stephenlam.me@gmail.com>
2021-04-08 18:46:04 -0700

    udpate (backend): include assigned techs for a ticket in /api/ticket

--- Server/helpdesk.js
+++ Server/helpdesk.js
@@ -50,19 +50,107 @@ app.get('/protected', validateAuth, function(req, res) {
 app.get('/api/tickets', validateAuth, function(req, res) {
 	console.log(`Getting tickets for userid: ${req.user.id}`)
 	
-	let queryOptions = req.query.filters  == undefined ? ({'query':'SELECT * FROM `ticket` WHERE `user_id` = ?', 'params':[req.user.id]}) : (function(filters){
-		console.log(`Applying filter: ${req.query.filters}`)
-		let string = 'SELECT * FROM `ticket` WHERE `user_id` = ?'
-		let params = [req.user.id]
-		if(filters.priority !== undefined){
-			string += ' AND `ticket_severity` = ?'
-			params.push(filters.priority)
+	let filters = Object.create(null);
+	if (req.query.filters) {
+		try {
+			const parsedFilters = JSON.parse(req.query.filters);
+			['priority', 'closed', 'date'].map((key) => {
+				if (key in Object.keys(parsedFilters)) {
+					if (key === 'date') {
+						const { start, end } = parsedFilters.date;
+						// TODO: verify start, end are actual dates.
+						filters[key] = [
+							connection.escape(start),
+							connection.escape(end),
+						];
+					} else {
+						filters[key] = connection.escape(parsedFilters[key]);
+					}
+				}
+			})
+			Object.freeze(filters);
+		} catch (_) {
+			return res.status(400).send({ message: 'Bad Request'});
+		}
 		}
 		
-		if(filters.company !== undefined)
-		{
+	const userId = connection.escape(req.user.id);
+	const query = `
+		select
+			t.ticket_id as id,
+			t.created,
+			t.title,
+			t.body,
+			t.status,
+			t.date_closed,
+			t.user_id,
+			t.ticket_severity,
+			JSON_ARRAYAGG(JSON_OBJECT(
+				'first_name', uta.first_name,
+				'last_name', uta.last_name,
+				'phone_number', uta.phone_number,
+				'email', uta.email
+			)) as assigned
+		from
+			ticket t
+			join (
+				select
+					ta.ticket_id,
+					user.first_name,
+					user.last_name,
+					user.phone_number,
+					user.email,
+					user.user_id,
+					ta.employee_id,
+					ta.end as assign_end
+				from ticket_assigned ta
+				join user on ta.employee_id = user.user_id
+			) uta using(ticket_id)
+		where (
+			${filters.date ? `\`created\` BETWEEN ${filters.date.start} and ${filters.date.end} and` : ''}
+			${filters.priority ? '`t.ticket_severity` = ' + filters.priority + ' and' : ''}
+			${filters.status ? '`t.status` = ' + filters.status + ' and' : ''}
+			(t.user_id = ${userId} or uta.employee_id = ${userId}) and
+			((t.date_closed <= now() and (uta.assign_end is null or uta.assign_end = t.date_closed)) or
+			(t.date_closed is null and (uta.assign_end is null or not uta.assign_end < now())))
+		)
+		group by t.ticket_id
+		union select
+			t2.ticket_id as id,
+			t2.created,
+			t2.title,
+			t2.body,
+			t2.status,
+			t2.date_closed,
+			t2.user_id,
+			t2.ticket_severity,
+			null
+		from
+			ticket t2
+			join (
+				select
+					ta.ticket_id,
+					count(employee_id) as assigned
+				from ticket_assigned ta
+				group by ticket_id
+				having assigned = 0
+			) unassigned using(ticket_id)
+		where (
+			${filters.date ? `\`created\` BETWEEN ${filters.date.start} and ${filters.date.end} and` : ''}
+			${filters.priority ? '`t2.ticket_severity` = ' + filters.priority + ' and' : ''}
+			${filters.status ? '`t2.status` = ' + filters.status + ' and' : ''}
+			t2.user_id = ${userId}
+		)
+		order by created
+	`;
 			
-		}
+	connection.query(query, (err, tickets) => {
+		res.status(200).json(Array.from(tickets, (t) => {
+			t.assigned = JSON.parse(t.assigned);
+			return t;
+		}));
+	});
+});
 		
 		if(filters.closed !== undefined)
 		{
