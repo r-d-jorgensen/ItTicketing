42da0463708af96fc35119c2933e8eb1de7da26b
Stephen Lam <stephenlam.me@gmail.com>
2021-04-19 16:15:47 -0700

    update (frontend): add select to change new ticket severity

--- Front-End/src/views/Customer/NewFormModal.jsx
+++ Front-End/src/views/Customer/NewFormModal.jsx
@@ -18,6 +18,7 @@ export default function NewFormModal({ show, setShow }) {
   const { token } = useAuth();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const [selectedSeverity, setSelectedSeverity] = useState(1);
 
   async function submitTicket(e) {
     e.preventDefault();
@@ -25,12 +26,22 @@ export default function NewFormModal({ show, setShow }) {
     headers.set('Content-Type', 'application/json');
     headers.set('Authorization', `Bearer ${token}`);
 
+    /* eslint-disable camelcase */
+    let ticket_severity = parseInt(selectedSeverity, 10) || 1;
+    if (ticket_severity < 1 || ticket_severity > 4) {
+      ticket_severity = 1;
+    }
+
     const reqOpts = {
       headers,
       method: 'POST',
-      body: JSON.stringify({ title, body, ticket_severity: 3 }),
+      body: JSON.stringify({
+        title,
+        body,
+        ticket_severity,
+      }),
     };
-
+    /* eslint-enable camelcase */
     try {
       const { id } = await request('/api/clientcreate', reqOpts);
       history.push(`/dashboard/ticket/${id}`);
@@ -42,15 +53,48 @@ export default function NewFormModal({ show, setShow }) {
   return (
     <div className={'it-nfm'.concat([show ? ' nfm--active' : ''])}>
       <div className="nfm-wrapper">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="nfm-close"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          onClick={() => {
+            setShow(false);
+            setTitle('');
+            setBody('');
+          }}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+        </svg>
         <span className="nfm-header">New Ticket</span>
         <form className="nfm-form" onSubmit={submitTicket}>
-          <label htmlFor="nfmTitle" className="nfm-label">Title</label>
-          <Input
-            id="nfmTitle"
-            name="Title"
-            value={title}
-            onChange={(e) => { setTitle(e.target.value); }}
-          />
+          <div className="nfm-group">
+            <div className="nfm-unit">
+              <label htmlFor="nfmTitle" className="nfm-label">Title</label>
+              <input
+                className="nfm-input"
+                id="nfmTitle"
+                name="Title"
+                value={title}
+                onChange={(e) => { setTitle(e.target.value); }}
+              />
+            </div>
+            <div className="nfm-unit">
+              <label htmlFor="nfmSeverity" className="nfm-label">Severity</label>
+              <select
+                id="nfmSeverity"
+                name="Severity"
+                className="nfm-select"
+                onChange={({ target: { value } }) => { setSelectedSeverity(value); }}
+              >
+                <option selected value={1}>Low</option>
+                <option value={2}>Mild</option>
+                <option value={3}>High</option>
+                <option value={4}>Urgent</option>
+              </select>
+            </div>
+          </div>
           <label htmlFor="nfmBody" className="nfm-label">Description</label>
           <textarea
             id="nfmBody"
@@ -59,17 +103,23 @@ export default function NewFormModal({ show, setShow }) {
             value={body}
             onChange={(e) => { setBody(e.target.value); }}
           />
-          <Button
-            type="submit"
-            disabled={title === '' || body === ''}
-          >
-            Submit
-          </Button>
+          <div className="nfm-group">
+            <Button
+              type="submit"
+              disabled={title === '' || body === ''}
+            >
+              Submit
+            </Button>
+          </div>
         </form>
       </div>
       <div
         className="nfm-background"
-        onClick={() => { setShow(false); }}
+        onClick={() => {
+          setShow(false);
+          setTitle('');
+          setBody('');
+        }}
       />
     </div>
   );
