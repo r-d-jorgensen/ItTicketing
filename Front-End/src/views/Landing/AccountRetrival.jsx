b6f88e1c834dbc87d97db8d2eb2458b67bf5df6f
Stephen Lam <stephenlam.me@gmail.com>
2021-04-22 14:41:01 -0700

    add (frontend): account retrieval page

--- Front-End/src/views/Landing/AccountRetrival.jsx
+++ Front-End/src/views/Landing/AccountRetrival.jsx
@@ -1,42 +1,52 @@
 import React, { Fragment } from 'react';
-import Navbar from 'components/Navbar';
+import { Link } from 'react-router-dom';
+
+import Button from 'components/Button';
+import Input from 'components/Input';
+
+import appStyles from '../../App.css';
 import styles from './AccountRetrival.css';
 
 function AccountRetrival() {
+  const submit = (type) => {
+    if (type === 'password') {
+      // send reset email link
+    } else if (type === 'username') {
+      // send username
+    }
+  };
+
   return (
     <Fragment>
-      <Navbar />
-      <main className={styles['account-retrival-page']}>
-        <div>
-          <h3>If Lost Password</h3>
-          <p>
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            <br />
-            <br />
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-          </p>
-        </div>
-        <div>
-          <h3>If Lost Username</h3>
-          <p>
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            <br />
-            <br />
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-            words about how to recover the username words about how to recover the username
-          </p>
+      <header>
+        <h1 className={appStyles.logo}>
+          <Link to="/">IT Ticketing Systems Inc.</Link>
+        </h1>
+      </header>
+      <main className={styles['account-retrieval-page']}>
+        <div className={styles['account-container']}>
+          <h2>Forgot Your Password or Username?</h2>
+          <form className={styles.form}>
+            <label htmlFor="emailInput">Email</label>
+            <Input
+              id="emailInput"
+              name="Email"
+            />
+            <div className={styles.group}>
+              <Button
+                type="submit"
+                onClick={() => { submit('password'); }}
+              >
+                Forgot Password
+              </Button>
+              <Button
+                type="submit"
+                onClick={() => { submit('username'); }}
+              >
+                Forgot Username
+              </Button>
+            </div>
+          </form>
         </div>
       </main>
     </Fragment>
